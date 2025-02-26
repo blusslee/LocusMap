1bc01726197d4d146d4e3d9ddda3db082a627bb9
blusslee <62461084+blusslee@users.noreply.github.com>
2025-02-26 19:03:23 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -8,17 +8,17 @@ module.exports = {
   // You can also put this in your worker's environment variable if you worry about
   // your email being exposed in public.
   // Format: i@example.com
-  userPrincipalName: process.env.USER_PRINCIPLE_NAME || '',
+  userPrincipalName: process.env.USER_PRINCIPLE_NAME || '46293262@comunidadort.edu.ar',
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: process.env.NEXT_PUBLIC_TITLE || "OnePortal",
+  title: process.env.NEXT_PUBLIC_TITLE || "户外资源",
 
   // The folder that you are to share publicly with OnePortal. Use '/' if you want to share your root folder.
-  baseDirectory: process.env.BASE_DIRECTORY || '/',
+  baseDirectory: process.env.BASE_DIRECTORY || '/root',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
